1f3781474584a70a7a8453ad073736abcfe3783d
Shane Jaroch <chown_tee@proton.me>
2024-12-15 04:05:09 -0500

    tidy

--- ford2024/cost_min.py
+++ ford2024/cost_min.py
@@ -1,8 +1,7 @@
 """
-Solve 3rd problem
+Solve 3rd problem with Anton.
 """
 
-import itertools
 import math
 from typing import Dict, List, Tuple
 
@@ -14,8 +13,11 @@ COSTS = [190, 200, 450, 870, 110, 499, 358, 160]
 WEEKS = 4
 # Solution: ('1,2,7', 1420)
 
+####################################################
 # EXAMPLE PROBLEMS
-# 3 weeks
+####################################################
+
+# 3 weeks, 6 costs (4 + 3 + 2 + 1 = 10 possibilities)
 # [190 | 200 | 450 499 358 160]
 # [190 | 200 450 | 499 358 160]
 # [190 | 200 450 499 | 358 160]
@@ -30,7 +32,7 @@ WEEKS = 4
 
 # [190 200 450 499 | 358 | 160]
 
-# 4 weeks
+# 4 weeks, 8 costs (35 possibilities)
 # [190 | 200 | 450 | 870 110 499 358 160]
 # [190 | 200 | 450 870 | 110 499 358 160]
 # [190 | 200 | 450 870 110 | 499 358 160]
@@ -55,6 +57,7 @@ WEEKS = 4
 # [190 200 | 450 | 870 110 | 499 358 160]
 # [190 200 | 450 | 870 110 499 | 358 160]
 # [190 200 | 450 | 870 110 499 358 | 160]
+# ...
 
 
 def calculate_sum(bars: List[int]) -> int:
@@ -69,15 +72,11 @@ def optimize() -> Tuple[str, int]:
     """Main solution method"""
     limits: List[int] = [len(COSTS) - (WEEKS - x) + 1 for x in range(WEEKS - 1)]
     bars = [x + 1 for x in range(WEEKS - 1)]
-    _combs = list(itertools.combinations((x for x in range(1, len(COSTS))), WEEKS - 1))
-    print(len(_combs))
-    for _c in _combs:
-        print(f"  {_c}")
     bars.insert(0, 0)
     bars.append(len(COSTS))
 
-    print("Limits  ", limits)
     print("Bars ", bars)
+    print("Limits  ", limits)
     print("Costs", COSTS)
     print()
 
@@ -92,7 +91,7 @@ def optimize() -> Tuple[str, int]:
         # Move bars
         finished = move_next(bars, limits)
 
-    print(f"{len(results)} results:")
+    print(f"{len(results)} partitions")
     # for k, v in results.items():
     #     print(f"    {k}: {v}")
 
@@ -118,8 +117,8 @@ def optimize_official_dynamic_programming_solution():
     https://leetcode.com/discuss/interview-question/5876966/LinkedIn-OA-2025-Summer-Intern
     """
     n = len(COSTS)
-    print(f"n{WEEKS}")
-    print(f"costs={COSTS}")
+    # print(f"n{WEEKS}")
+    # print(f"costs={COSTS}")
 
     # Initialize dp array
     dp = [[math.inf] * (WEEKS + 1) for _ in range(n + 1)]
@@ -140,31 +139,32 @@ def optimize_official_dynamic_programming_solution():
 
     # For each week
     for w in range(0, WEEKS):
-        print(f"w={w}")
+        # print(f"w={w}")
         # for each of the cost
         for i in range(1, n + 1):
-            print(f"  i={i}")
+            # print(f"  i={i}")
             max_cost_in_week = 0
 
             for k in range(i - 1, -1, -1):
                 # Compute the maximum cost within the current subarray
                 # being considered for the week
                 max_cost_in_week = max(max_cost_in_week, COSTS[k])
-                print(f"    k={k}, c={COSTS[k]}, maxc={max_cost_in_week}", end="")
+                # print(f"    k={k}, c={COSTS[k]}, maxc={max_cost_in_week}", end="")
 
                 # Ensure the previous state is valid before using it in the computation
                 if dp[k][w] != math.inf:
                     dp[i][w + 1] = min(dp[i][w + 1], dp[k][w] + max_cost_in_week)
-                    print(f", dp={dp[i]}")
+                    # print(f", dp={dp[i]}")
                 else:
-                    print()
+                    # print()
+                    pass
 
-    for p in dp:
-        print(f"p={p}")
+    # for p in dp:
+    #     print(f"p={p}")
     return int(dp[n][WEEKS])
 
 
 print()
 
 print(f"Solution: {optimize()}")
-# print(optimize_official_dynamic_programming_solution())
+print(f"Official: {optimize_official_dynamic_programming_solution()}")
